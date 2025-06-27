c3372337a280c44cf7801357ebdc5ff9e3744cd8
treychaffin <trey@wildcat-fab.com>
2025-06-27 15:33:45 -0500

    Refactor docstring and improve logging in Renogy charge controller script

--- renogy.py
+++ renogy.py
@@ -1,5 +1,5 @@
 #! .venv/bin/python
-"""tools for retrieving data from a Renogy charge controller."""
+"""script for testing Renogy charge controller."""
 
 import datetime
 import logging
@@ -11,24 +11,23 @@ log = logging.getLogger(__name__)
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
-    from pprint import pprint
     # Example usage
-    cc = RenogyChargeController(portname="/dev/ttyUSB0",slaveaddress=1)
+    cc = RenogyChargeController(portname="/dev/ttyUSB0", slaveaddress=1)
     status = {
-        "timestamp" : datetime.datetime.now().isoformat(),
-        "solar_voltage" : cc.get_solar_voltage(),
-        "solar_current" : cc.get_solar_current(),
-        "solar_power" : cc.get_solar_power(),
-        "load_voltage" : cc.get_load_voltage(),
-        "load_current" : cc.get_load_current(),
-        "load_power" : cc.get_load_power(),
-        "battery_voltage" : cc.get_battery_voltage(),
-        "battery_state_of_charge" : cc.get_battery_state_of_charge(),
-        "battery_temperature" : cc.get_battery_temperature(),
-        "controller_temperature" : cc.get_controller_temperature(),
-        "maximum_solar_power_today" : cc.get_maximum_solar_power_today(),
-        "minimum_solar_power_today" : cc.get_minimum_solar_power_today(),
-        "maximum_battery_voltage_today" : cc.get_maximum_battery_voltage_today(),
-        "minimum_battery_voltage_today" : cc.get_minimum_battery_voltage_today()
+        "timestamp": datetime.datetime.now().isoformat(),
+        "solar_voltage": cc.get_solar_voltage(),
+        "solar_current": cc.get_solar_current(),
+        "solar_power": cc.get_solar_power(),
+        "load_voltage": cc.get_load_voltage(),
+        "load_current": cc.get_load_current(),
+        "load_power": cc.get_load_power(),
+        "battery_voltage": cc.get_battery_voltage(),
+        "battery_state_of_charge": cc.get_battery_state_of_charge(),
+        "battery_temperature": cc.get_battery_temperature(),
+        "controller_temperature": cc.get_controller_temperature(),
+        "maximum_solar_power_today": cc.get_maximum_solar_power_today(),
+        "minimum_solar_power_today": cc.get_minimum_solar_power_today(),
+        "maximum_battery_voltage_today": cc.get_maximum_battery_voltage_today(),
+        "minimum_battery_voltage_today": cc.get_minimum_battery_voltage_today(),
     }
-    pprint(status)
+    log.info(status)
