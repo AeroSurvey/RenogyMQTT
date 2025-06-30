826c61e8e37a225508ebbe52a1f59525fd5c5978
treychaffin <trey@wildcat-fab.com>
2025-06-30 15:29:33 -0500

    Add additional register to try in find_slave_address for improved address detection

--- find_USB_parameters.py
+++ find_USB_parameters.py
@@ -29,7 +29,7 @@ def find_slave_address(portname: str, verbose: bool = False) -> int:
 
     addresses = []
 
-    registers_to_try = [(0x1402, 8), (0x000C, 16)]
+    registers_to_try = [(0x1402, 8), (0x000C, 16), (0x00C, 8)]
 
     for address in range(0x01, 0xFF):
         if verbose:
