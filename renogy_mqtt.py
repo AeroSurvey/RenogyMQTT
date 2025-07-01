024f3388583981e488872528df7a6da5e18b237e
treychaffin <trey@wildcat-fab.com>
2025-07-01 08:39:17 -0500

    Refactor data topic initialization to use the base topic structure

--- renogy_mqtt.py
+++ renogy_mqtt.py
@@ -51,7 +51,7 @@ class RenogyChargeControllerMQTTClient(MQTTClient):
         self.controller_type = self.charge_controller.get_controller_type()
 
         super().__init__(broker, port, name, base_topic="solar")
-        self.data_topic = "data"
+        self.data_topic = f"{self.topic}/data"
 
     def status_message(self, status: bool) -> dict:
         """Create a status message for the MQTT topic."""
